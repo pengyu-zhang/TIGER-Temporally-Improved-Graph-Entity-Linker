18faf4798d2671118b182140ce07e7bbb7279a42
Pengyu Zhang <110645502+pengyu-zhang@users.noreply.github.com>
2023-09-04 15:32:17 +0200

    Update train.sh

--- train.sh
+++ train.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-PYTHONPATH=. python blink/biencoder/train_biencoder.py \
+PYTHONPATH=. python TIGER/biencoder/train_biencoder.py \
   --data_path data/01_blink_baseline/blink_format/random_sample/2013/ \
-  --output_path /gpfs/scratch1/nodespecific/int5/pzhang/models/01_blink_baseline_2013_train_random_test_new/biencoder \
+  --output_path /models/01_blink_baseline_2013_train_random_test_new/biencoder \
   --learning_rate 1e-05 --num_train_epochs 1 --max_context_length 128 --max_cand_length 128 \
   --train_batch_size 64 --eval_batch_size 16 --bert_model google/bert_uncased_L-8_H-512_A-8 \
   --type_optimization all_encoder_layers --data_parallel \
@@ -19,10 +19,10 @@ PYTHONPATH=. python blink/biencoder/train_biencoder.py \
   --dropout 0.5 \
 
 # Get top-64 predictions from Biencoder model on train, valid and test dataset
-PYTHONPATH=. python blink/biencoder/eval_biencoder.py \
-  --path_to_model /gpfs/scratch1/nodespecific/int5/pzhang/models/01_blink_baseline_2013_train_random_test_new/biencoder/pytorch_model.bin \
+PYTHONPATH=. python TIGER/biencoder/eval_biencoder.py \
+  --path_to_model /models/01_blink_baseline_2013_train_random_test_new/biencoder/pytorch_model.bin \
   --data_path data/01_blink_baseline/blink_format/new_entities/ \
-  --output_path /gpfs/scratch1/nodespecific/int5/pzhang/models/01_blink_baseline_2013_train_random_test_new/ \
+  --output_path /models/01_blink_baseline_2013_train_random_test_new/ \
   --encode_batch_size 8 --eval_batch_size 2 --top_k 64 --save_topk_result \
   --bert_model google/bert_uncased_L-8_H-512_A-8 --mode 2013,2014,2015,2016,2017,2018,2019,2020,2021,2022 \
   --zeshel True --data_parallel \
